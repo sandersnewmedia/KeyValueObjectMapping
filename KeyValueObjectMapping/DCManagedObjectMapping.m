0fe2757ad6db7cdda2d93023ead76823808f79e6
Scott Meisburger <smeisburger@gmail.com>
2012-09-20 19:49:02 -0400

    added strategy for merging multiple turns in context

--- KeyValueObjectMapping/DCManagedObjectMapping.m
+++ KeyValueObjectMapping/DCManagedObjectMapping.m
@@ -6,6 +6,7 @@
 
 #import "DCManagedObjectMapping.h"
 #import "DCDynamicAttribute.h"
+#import "Turn.h"
 
 
 @implementation DCManagedObjectMapping {
@@ -40,6 +41,20 @@
     return object;
 }
 
+// Helpers for debugging duplicate objs w/ same primary key
+- (void)deleteEntitiesInCollection:(NSArray *)collection
+{
+    for (NSManagedObject *obj in collection) {
+        TFLog(@"(findObjectByPrimaryKeyValue): deleting: %@", obj);
+        [obj deleteInContext:context];
+    }
+}
+
+- (id)returnWithTFLog:(id)returnValue
+{
+    TFLog(@"(findObjectByPrimaryKeyValue): returning: %@", returnValue);
+    return returnValue;
+}
 
 - (id)findObjectByPrimaryKeyValue:(id)primaryKeyValue
 {
@@ -51,24 +66,65 @@
     request.predicate = predicate;
 
     NSError *error;
-    NSArray *objects = [context executeFetchRequest:request error:&error];
+    NSMutableArray *objects = [NSMutableArray arrayWithArray:[context executeFetchRequest:request error:&error]];
     if (error) {
         NSLog(@"%@", [error localizedDescription]);
         return nil;
     }
 
     if (objects.count > 1) {
-        TFLog(@"(findObjectByPrimaryKeyValue): detected %i %@s in context with %@: %@ --lastObject will be returned",
+        TFLog(@"(findObjectByPrimaryKeyValue): detected %i %@s in context with %@: %@ --deciding what to do",
                 objects.count,
                 NSStringFromClass(self.classToGenerate),
                 primaryKeyAttribute.objectMapping.attributeName,
                 primaryKeyValue);
-        TFLog(@"%@", objects);
+        TFLog(@"(findObjectByPrimaryKeyValue): objects: %@", objects);
+
+        // Separate Turn objects that have turnPrev from array of all objects
+        NSMutableArray *turnsWithTurnPrev = [[NSMutableArray alloc] init];
+
+        for (NSManagedObject *obj in objects) {
+            if ([obj isKindOfClass:[Turn class]]) {
+                if ([(Turn *)obj turnPrev]) {
+                    [turnsWithTurnPrev addObject:obj];
+                }
+            }
+        }
+
+        for (Turn *turn in turnsWithTurnPrev) {
+            if ([objects containsObject:turn]) {
+                [objects removeObject:turn];
+            }
+        }
+
+        // Make decision about which Turn to return
+        if (turnsWithTurnPrev.count == 1) {
+            TFLog(@"(findObjectByPrimaryKeyValue): Scenario 1: one duplicate Turn has turnPrev --returing this, deleting all others");
+            [self deleteEntitiesInCollection:objects];
+            [self returnWithTFLog:[turnsWithTurnPrev lastObject]];
+        }
+        
+        if (turnsWithTurnPrev.count == 0) {
+            TFLog(@"(findObjectByPrimaryKeyValue): Scenario 2: no duplicate Turns have turnPrev --returning lastObject, deleting all others");
+            NSManagedObject *objectToReturn = [objects lastObject];
+            [objects removeLastObject];
+            
+            [self deleteEntitiesInCollection:objects];
+            [self returnWithTFLog:objectToReturn];
+        }
+        
+        if (turnsWithTurnPrev.count > 1) {
+            TFLog(@"(findObjectByPrimaryKeyValue): Scenario 3: more than one duplicate Turn has turnPrev --returning last of these, deleting all others");
+            Turn *turnToReturn = [turnsWithTurnPrev lastObject];
+            [turnsWithTurnPrev removeLastObject];
+            
+            [self deleteEntitiesInCollection:turnsWithTurnPrev];
+            [self deleteEntitiesInCollection:objects];
+            [self returnWithTFLog:turnToReturn];
+        }
     }
 
     return [objects lastObject];
-
-
 }
 
 @end
